8daf13a238388c90c21d34a50e517068aec246a7
Mac <ftrcsg@gmail.com>
2025-08-23 21:17:41 -0300

    fix(02): add environment configs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,22 @@
+import {faBluesky, faDiscord, faTwitter} from '@fortawesome/free-brands-svg-icons';
+
+export const environment = {
+  production: false,
+  socialLinks: [
+    {
+      url: 'https://bsky.app/profile/mcnt.sh',
+      faIcon: faBluesky,
+      enabled: true,
+    },
+    {
+      url: 'https://x.com/mac_floof',
+      faIcon: faTwitter,
+      enabled: true,
+    },
+    {
+      url: 'https://discord.gg/EyakfCKnCY',
+      faIcon: faDiscord,
+      enabled: true,
+    },
+  ]
+};
